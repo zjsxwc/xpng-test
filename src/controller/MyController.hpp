4b1447d0acb89449380c831805fd7fd40fdb057c
wangchao <watcher.hangzhou@foxmail.com>
2019-09-23 17:05:34 +0800

    fix

---
+++ src/controller/MyController.hpp
@@ -0,0 +1,178 @@
+//
+//  MyController.hpp
+//  web-starter-project
+//
+//  Created by Leonid on 2/12/18.
+//  Copyright © 2018 oatpp. All rights reserved.
+//
+
+#ifndef MyController_hpp
+#define MyController_hpp
+
+#include "dto/DTOs.hpp"
+
+#include "oatpp/web/server/api/ApiController.hpp"
+#include "oatpp/core/macro/codegen.hpp"
+#include "oatpp/core/macro/component.hpp"
+
+
+#include <iostream>
+#include <oatpp/web/protocol/http/outgoing/ChunkedBufferBody.hpp>
+
+static const std::string base64_chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789+/";
+
+
+static inline bool is_base64(unsigned char c) {
+    return (isalnum(c) || (c == '+') || (c == '/'));
+}
+
+std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
+    std::string ret;
+    int i = 0;
+    int j = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
+
+    while (in_len--) {
+        char_array_3[i++] = *(bytes_to_encode++);
+        if (i == 3) {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
+
+            for(i = 0; (i <4) ; i++)
+                ret += base64_chars[char_array_4[i]];
+            i = 0;
+        }
+    }
+
+    if (i)
+    {
+        for(j = i; j < 3; j++)
+            char_array_3[j] = '\0';
+
+        char_array_4[0] = ( char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+
+        for (j = 0; (j < i + 1); j++)
+            ret += base64_chars[char_array_4[j]];
+
+        while((i++ < 3))
+            ret += '=';
+
+    }
+
+    return ret;
+
+}
+
+std::string base64_decode(std::string const& encoded_string) {
+    size_t in_len = encoded_string.size();
+    int i = 0;
+    int j = 0;
+    int in_ = 0;
+    unsigned char char_array_4[4], char_array_3[3];
+    std::string ret;
+
+    while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
+        char_array_4[i++] = encoded_string[in_]; in_++;
+        if (i ==4) {
+            for (i = 0; i <4; i++)
+                char_array_4[i] = base64_chars.find(char_array_4[i]) & 0xff;
+
+            char_array_3[0] = ( char_array_4[0] << 2       ) + ((char_array_4[1] & 0x30) >> 4);
+            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) +   char_array_4[3];
+
+            for (i = 0; (i < 3); i++)
+                ret += char_array_3[i];
+            i = 0;
+        }
+    }
+
+    if (i) {
+        for (j = 0; j < i; j++)
+            char_array_4[j] = base64_chars.find(char_array_4[j]) & 0xff;
+
+        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+
+        for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+    }
+
+    return ret;
+}
+
+
+
+
+/**
+ * Sample Api Controller.
+ */
+class MyController : public oatpp::web::server::api::ApiController {
+public:
+    /**
+     * Constructor with object mapper.
+     * @param objectMapper - default object mapper used to serialize/deserialize DTOs.
+     */
+    MyController(OATPP_COMPONENT(std::shared_ptr<ObjectMapper>, objectMapper))
+            : oatpp::web::server::api::ApiController(objectMapper) {}
+
+public:
+
+/**
+ *  Begin ENDPOINTs generation ('ApiController' codegen)
+ */
+#include OATPP_CODEGEN_BEGIN(ApiController)
+
+    ENDPOINT("GET", "/", root) {
+        auto dto = MyDto::createShared();
+        dto->statusCode = 200;
+        dto->message = "Hello World!";
+        return createDtoResponse(Status::CODE_200, dto);
+    }
+
+
+
+
+
+
+
+
+
+
+    ENDPOINT("GET", "/xpng", xpng) {
+        auto onepointpngBase64 = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWP4////fwAJ+wP9CNHoHgAAAABJRU5ErkJggg==";
+        auto imageDecoded = base64_decode(onepointpngBase64);
+        auto imageBin = imageDecoded.c_str();
+
+        auto stream = oatpp::data::stream::ChunkedBuffer::createShared();
+        auto response = oatpp::web::protocol::http::outgoing::Response::createShared
+                (Status::CODE_200, oatpp::web::protocol::http::outgoing::ChunkedBufferBody::createShared(stream));
+
+        stream->write(imageBin, imageDecoded.length());
+        response->putHeaderIfNotExists("Content-type", "image/png");
+
+        return response;
+    }
+
+
+
+
+
+
+    // TODO Insert Your endpoints here !!!
+
+/**
+ *  Finish ENDPOINTs generation ('ApiController' codegen)
+ */
+#include OATPP_CODEGEN_END(ApiController)
+
+};
+
+#endif /* MyController_hpp */
